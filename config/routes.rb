ab8ba4e6d7023b14fc039ccdbd04303ab7dbe405
Kelvin Parian <pariank@icloud.com>
2021-02-04 11:12:25 -0800

    Fix route syntax for "resource user".

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   get 'signup', to: 'users#new'
-  resource 'users', except: [:new]
+  resources :users, except: [:new]
 end
